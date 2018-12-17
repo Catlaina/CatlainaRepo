5220be183ea6839a3ef70ec38de85d2e36cf2977
lemarc81 <mr.lehmann@gmail.com>
2018-12-17 22:47:01 +0100

    refactored fields to properties

--- frontend/Catlaina/src/app/workshop-activity/workshop-activity.component.ts
+++ frontend/Catlaina/src/app/workshop-activity/workshop-activity.component.ts
@@ -8,28 +8,51 @@ import { Component, OnInit } from '@angular/core';
 export class WorkshopActivityComponent implements OnInit {
 
   private _title:string;
-  private _durationMinutes:number;
-  
   public get title() {
     return this._title;
   }
-  public set title(value) {
-    this._title = value;
-  }
-  
+  // public set title(value) {
+  //   this._title = value;
+  // }
+
+  private _durationMinutes:number;
   public get durationMinutes() {
     return this._durationMinutes;
   }
-  public set durationMinutes(value) {
-    this._durationMinutes = value;
+  // public set durationMinutes(value) {
+  //   this._durationMinutes = value;
+  // }
+  
+  private _agendaDesctiption:string;
+  public get agendaDesctiption() {
+    return this._agendaDesctiption;
   }
-  agendaDesctiption = "Activity blablabla";
-  facilitationNotes = "Lorem Ipsum dolor sit Amet";
-  requiredMaterial = "2 FlipCharts, 1 Whiteboard, 1 Pinboard";
+  // public set agendaDesctiption(value) {
+  //   this._agendaDesctiption = value;
+  // }
+
+  private _facilitationNotes:string;
+  public get facilitationNotes() {
+    return this._facilitationNotes;
+  }
+  // public set facilitationNotes(value) {
+  //   this._facilitationNotes = value;
+  // }
+  
+  private _requiredMaterial:string;
+  public get requiredMaterial() {
+    return this._requiredMaterial;
+  }
+  public set requiredMaterial(value) {
+    this._requiredMaterial = value;
+  }
 
   constructor() { 
     this._title = "Activity Title";
     this._durationMinutes  = 90;
+    this._agendaDesctiption = "Activity blablabla";
+    this._facilitationNotes  = "Lorem Ipsum dolor sit Amet";
+    this._requiredMaterial = "2 FlipCharts, 1 Whiteboard, 1 Pinboard";
   }
 
   ngOnInit() {
